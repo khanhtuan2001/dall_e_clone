2a37673d3dfdfd541bfb78b2d9d3cfd39753923f
khanhtuan <khanhtuannguyen2001@gmail.com>
2023-03-18 01:06:04 +0700

    keep going

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import * as dotenv from 'dotenv';
 import cors from 'cors';
 
+import connectDB from "./monodb/connect.js";
 dotenv.config();
 
 const app = express();
@@ -15,7 +16,13 @@ app.get('/', async(req, res) => {
 })
 
 const startServer = async() => {
-    app.listen(8080, () => console.log('server has started on port http://localhost:8080'))
 
+    try {
+        connectDB(process.env.MONGODB_URL)
+
+        app.listen(8080, () => console.log('server has started on port http://localhost:8080'))
+    } catch (error) {
+        console.log(error);
+    }
 }
 startServer();
